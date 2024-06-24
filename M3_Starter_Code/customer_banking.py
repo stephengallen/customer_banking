d5f9ab9174e529d680c277c523924e9d40d2ddab
Stephen Gallen <sgallen9@gmail.com>
2024-06-24 13:41:38 -0700

    third commit

--- M3_Starter_Code/customer_banking.py
+++ M3_Starter_Code/customer_banking.py
@@ -1,5 +1,6 @@
 # Import the create_cd_account and create_savings_account functions
-# ADD YOUR CODE HERE
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 
 # Define the main function
 def main():
@@ -8,13 +9,16 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    # ADD YOUR CODE HERE
+    savings_balance = float(input("Enter your initial savings account balance: "))
+    savings_interest_rate = float(input("Enter the annual interest rate for the savings account"))
+    savings_months = int(input("Enter the number of months for the savings account: "))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    # ADD YOUR CODE HERE
+    print(f"Savings Account - Interest Earned: {savings_interest_earned:.2f}")
+    print(f"Savings Account - Updated Balance: {savings_updated_balance:.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
